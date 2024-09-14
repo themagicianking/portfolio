c1f46289f19863a6f8fc07968ad5a9569ad4891b
Thane Chaves <tmoonder@gmail.com>
2024-09-14 01:04:20 -0400

    Placed tabs on the right side of the page.

--- src/components/Sections.jsx
+++ src/components/Sections.jsx
@@ -47,6 +47,7 @@ export default function VerticalTabs() {
         flexGrow: 1,
         bgcolor: "background.paper",
         display: "flex",
+        flexDirection: "row-reverse",
         height: 224,
       }}
     >
