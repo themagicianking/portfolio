ebba66c3123bff38197348c36d52d53d0e5d52b8
Thane Chaves <tmoonder@gmail.com>
2024-09-13 23:16:05 -0400

    Converted project to React.

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>Hi, I'm</p>
+      <h1>Thane</h1>
+      <p>full stack software engineer</p>
+      <nav>
+        <div id="projects">
+          <h2>Projects</h2>
+          <p>Projects will go here.</p>
+        </div>
+        <div id="about">
+          <h2>About</h2>
+          <p>About section will go here.</p>
+        </div>
+        <div id="contact">
+          <h2>Contact</h2>
+          <p>Contact form will go here.</p>
+        </div>
+        <button>Projects</button>
+        <button>About</button>
+        <button>Contact</button>
+      </nav>
+    </>
+  );
+}
+
+export default App;
