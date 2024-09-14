c43f3ec6951823297852d326a36e2147926d2628
Thane Chaves <tmoonder@gmail.com>
2024-09-14 01:14:08 -0400

    Added custom fonts.

--- README.md
+++ README.md
@@ -7,9 +7,9 @@ A personal portfolio site.
 A site with a project, about, and contact section. The project section should include titles and descriptions for at least three projects with links to each project. The about section should contain a short blurb and a photo. The contact section should contain a submittable form that sends the given message to my personal email.
 
 ## Stories
-- Functional tabs for each section of the portfolio
+- Functional tabs for each section of the portfolio [x]
 - Custom font for each text element
-- Dynamically added projects
+- Dynamically added projects [x]
 - CSS animation for h1
 - CSS fade in animation for each section
 - The ability to expand and collapse each project
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import "./App.css";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import ProjectList from "./components/ProjectList";
+import Sections from "./components/Sections";
 
 function App() {
   return (
@@ -9,31 +7,7 @@ function App() {
       <p>Hi, I'm</p>
       <h1>Thane</h1>
       <p>full stack software engineer</p>
-      <Tabs orientation="vertical">
-        <Tab label="Projects">
-          <ProjectList />
-        </Tab>
-        <Tab label="About"></Tab>
-        <Tab label="Contact"></Tab>
-      </Tabs>
-      {/* <nav>
-        <div id="projects" className="section" style={{ display: "none" }}>
-          <h2>Projects</h2>
-          <ProjectList />
-        </div>
-        <div id="about" className="section" style={{ display: "none" }}>
-          <h2>About</h2>
-          <p>About section will go here.</p>
-        </div>
-        <div id="contact" className="section" style={{ display: "none" }}>
-          <h2>Contact</h2>
-          <p>Contact form will go here.</p>
-        </div>
-        <button>Home</button>
-        <button>Projects</button>
-        <button>About</button>
-        <button>Contact</button>
-      </nav> */}
+      <Sections />
     </>
   );
 }
